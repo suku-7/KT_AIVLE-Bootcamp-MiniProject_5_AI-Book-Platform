adda7c04007e57efbd2d7882737d483cab6ca14a
suyeon2 Park <suyeonp180@gmail.com>
2025-07-02 02:22:03 +0000

    플랫폼컨트롤러 수정

--- outside/src/main/java/thminiprojthebook/infra/PolicyHandler.java
+++ outside/src/main/java/thminiprojthebook/infra/PolicyHandler.java
@@ -3,14 +3,13 @@ package thminiprojthebook.infra;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javax.naming.NameParser;
-import javax.naming.NameParser;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 import thminiprojthebook.config.kafka.KafkaProcessor;
-import thminiprojthebook.domain.*;
+//import thminiprojthebook.domain.*;
 
 //<<< Clean Arch / Inbound Adaptor
 @Service
