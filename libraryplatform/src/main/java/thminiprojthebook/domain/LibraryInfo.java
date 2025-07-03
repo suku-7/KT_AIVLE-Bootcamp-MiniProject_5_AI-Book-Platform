54476e3ea83c87ac7d04fa5b0b112d7fd1b99a20
MistyClawn <74577734+Mistyclawn@users.noreply.github.com>
2025-07-03 11:16:29 +0900

    수연님 코드

--- libraryplatform/src/main/java/thminiprojthebook/domain/LibraryInfo.java
+++ libraryplatform/src/main/java/thminiprojthebook/domain/LibraryInfo.java
@@ -28,7 +28,6 @@ public class LibraryInfo {
 
     private String title;
 
-    @Column(length = 1000)
     private String imageUrl;
 
     private String summary;
@@ -58,6 +57,12 @@ public class LibraryInfo {
             this.selectCount = 0L; // Long 타입이므로 0L로 초기화
         }
         this.selectCount++;
+
+        // 추가: 베스트셀러 조건 체크 (5회 이상 시)
+    if (this.selectCount >= 5) {
+        this.bestseller = true; // 베스트셀러로 설정
+        new BestsellerGiven(this).publishAfterCommit(); // 이벤트 발행
+    }
     }
 
     //<<< Clean Arch / Port Method
@@ -81,7 +86,7 @@ public class LibraryInfo {
 
     //>>> Clean Arch / Port Method
     //<<< Clean Arch / Port Method
-   public static void publish(AiSummarized aiSummarized) {
+   public static LibraryInfo publish(AiSummarized aiSummarized) {
         /**
          * AI 서비스가 문서 요약을 완료하면, 그 내용을 바탕으로
          * 새로운 LibraryInfo(도서 정보)를 생성하고 Published 이벤트를 발행합니다.
@@ -93,10 +98,11 @@ public class LibraryInfo {
         // 2. AiSummarized 이벤트에 담겨온 정보로 객체의 속성을 설정합니다.
         libraryInfo.setBookId(aiSummarized.getBookId());
         libraryInfo.setAuthorId(aiSummarized.getAuthorId());
-        // libraryInfo.setAuthorName(aiSummarized.getAuthorName()); // 이벤트에 authorName도 있다면 추가
+        libraryInfo.setAuthorName(aiSummarized.getAuthorName()); // 이벤트에 authorName도 있다면 추가
         libraryInfo.setTitle(aiSummarized.getTitle()); // 이벤트에 title도 있다면 추가
         libraryInfo.setSummary(aiSummarized.getSummary()); // AI가 생성한 요약
         libraryInfo.setContext(aiSummarized.getContext()); // AI가 분석한 원문
+        libraryInfo.setClassificationType(aiSummarized.getClassificationType());
         libraryInfo.setPublishDate(new Date()); // 현재 시간으로 발행일 설정
         libraryInfo.setSelectCount(0L); // 최초 생성 시 선택 횟수는 0
         
@@ -107,10 +113,16 @@ public class LibraryInfo {
         Published published = new Published(libraryInfo);
         published.publishAfterCommit();
 
+    
         System.out.println(
             "publish(AiSummarized) Policy: A new book has been published." +
             " bookId: " + libraryInfo.getBookId()
         );
+        
+        return libraryInfo;
+
+
+        
     }
 
     //>>> Clean Arch / Port Method
